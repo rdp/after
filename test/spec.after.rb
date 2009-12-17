fa862f4c9e3ad28fe8f9ee79ac58b4c375d7675a
rdp <rogerpack2005@gmail.com>
2009-12-16 18:01:33 -0700

    I think it passes another one

--- test/spec.after.rb
+++ test/spec.after.rb
@@ -4,22 +4,29 @@ require_rel './after'
 
 describe After do
 
+  def go
+     pid = Process.spawn "ruby ./sleep_1.rb"
+  end
+
   it "should be able to grab the right pid" do
-     pid = Process.spawn "ruby ./sleep_10.rb"
-     sleep 0.01
-     a = After.find_pid('sleep_10')
-     a.should == pid
+     pid = go
+     a = After.find_pids('sleep_1')
+     a[0].should == pid
   end
 
   it "should immediately return if the other process doesn't exist" do
-    pending
+    a = After.find_pids('non existent process')
+    assert a.empty?
   end
 
   it "should wait for another process to terminate" do
-    pending
+    go
+    start = Time.now
+    After.find_and_wait_for('sleep_1')
+    assert (Time.now - start) < 0.5 
   end
 
-  it "should warn if there are several available" do
+  it "should work if there are several available" do
     pending
   end
 
